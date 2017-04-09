c5469d130585e7424466b5b41533360bc102285b
enguerrandgrx <enguerrand.granoux@epfl.ch>
2017-04-09 18:10:18 +0200

    set up color

--- project/grid/grid_fshader.glsl
+++ project/grid/grid_fshader.glsl
@@ -2,8 +2,9 @@
 
 in vec2 uv;
 in vec3 pos_3d;
+in float height; 
 
-out vec3 color;
+out vec4 color;
 
 uniform sampler2D tex;
 uniform mat4 MV;
@@ -11,13 +12,22 @@ uniform vec3 light_pos;
 
 
 void main() {
+
+    // Set up some useful color for interpolation
+    float alpha = 1.0; 
+    vec4 white = vec4(1.0,1.0,1.0,1.0); 
+    vec4 blue = vec4(0.0,0.0,1.0,1.0); 
+    vec4 darken_blue = vec4(0.1, 0.1,0.6,0.1); 
+    vec4 green = vec4(0.0, 0.7, 0.0, 1.0); 
+    vec4 tmp_color = vec4(0.0, 0.0, 0.0, 0.0); 
+
     vec3 v1 = dFdx(pos_3d);
     vec3 v2 = dFdy(pos_3d);
 
     vec3 normal = normalize(cross(v1, v2));
 
     // Setting ambient light
-    vec3 color_tmp = vec3(0.1f, 0.1f, 0.1f);
+    vec4 color_tmp = vec4(0.1f, 0.1f, 0.1f, alpha);
 
     vec3 light_dir = normalize(light_pos - vec3(MV * vec4(pos_3d, 1.0f)));
 
@@ -27,8 +37,22 @@ void main() {
     vec3 kd = vec3(0.557f, 0.33f, 0.204f);
     vec3 Ld = vec3(1.0f);
 
-    if(lambert > 0.0f)
-	color = color_tmp + Ld*kd*lambert;
-    else
+    if(lambert > 0.0){
+	color = color_tmp + vec4(Ld*kd*lambert,alpha);
+    }
+    else{
     	color = color_tmp;
+    }
+    
+    // Set up color in function of the height 
+    if(height > 1.0){
+        color = mix(white,color,0.1); 
+    }
+    else if(height < 0.75){
+        color = mix(darken_blue, blue, (height/1.2));
+    }
+    else {
+        tmp_color = mix(green, white, (height/1.2)); 
+        color = mix(tmp_color, color, 0.1); 
+    }
 }
--- project/grid/grid_vshader.glsl
+++ project/grid/grid_vshader.glsl
@@ -6,6 +6,7 @@ in vec2 position;
 
 out vec2 uv;
 out vec3 pos_3d;
+out float height; 
 
 uniform sampler2D tex;
 uniform mat4 MVP;
@@ -16,7 +17,7 @@ void main() {
   uv = (position + vec2(1.0, 1.0)) * 0.5;
   // Convert the 2D position into 3D positions that all lay 
   // in a horizontal plane
-  float height = texture(tex, uv).r;
+  height = texture(tex, uv).r;
   pos_3d = vec3(position.x, height, -position.y);
 
   gl_Position = MVP * vec4(pos_3d, 1.0);
